af1430bbf876714c534f1b26439c7913ba00d041
MilanMoreno <st_sra@protonmail.com>
2025-04-10 22:40:28 +0200

    Update Angular to v18 and fix vulnerabilities

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './core/routing/routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { createTranslationLoader } from './core/i18n/translation.loader';
